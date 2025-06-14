ea03529c5d720c1b85771f339daf579f7102a2c2
Alexey <jakobiannn@idlouc.com>
2025-06-14 21:56:53 +0300

    init

--- src/test/kotlin/sharding/storage/itmo/integration/MasterNodeApiTests.kt
+++ src/test/kotlin/sharding/storage/itmo/integration/MasterNodeApiTests.kt
@@ -5,8 +5,9 @@ import io.restassured.http.ContentType
 import org.junit.jupiter.api.*
 import org.hamcrest.Matchers.*
 import org.springframework.boot.test.context.SpringBootTest
-import sharding.storage.itmo.integration.model.ChangeShardRequest
-import sharding.storage.itmo.integration.model.NodeRequest
+
+data class NodeRequest(val address: String)
+data class ChangeShardRequest(val shardCount: Int)
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest
@@ -30,39 +31,50 @@ class MasterNodeApiTests {
     }
 
     @Test
-    fun addStorageNode() {
-        val node = NodeRequest(address = "localhost:8085")
-        RestAssured
-            .given()
-            .contentType(ContentType.JSON)
-            .body(node)
-            .post("/scheme")
-            .then()
-            .statusCode(200)
-            .body("message", containsString("done"))
-    }
-
-    @Test
-    fun removeStorageNode() {
-        // Ensure node exists before trying to remove
-        val node = NodeRequest(address = "localhost:8086")
-        RestAssured
-            .given()
+    fun addStorageNodeActuallyUpdatesScheme() {
+        val address = "localhost:8085"
+        val node = NodeRequest(address = address)
+        // Добавляем ноду
+        RestAssured.given()
             .contentType(ContentType.JSON)
             .body(node)
             .post("/scheme")
             .then()
             .statusCode(200)
 
-        RestAssured
-            .delete("/scheme/localhost:8086")
+        // Проверяем, что она появилась в схеме
+        RestAssured.get("/scheme")
             .then()
             .statusCode(200)
-            .body("message", containsString("done"))
+            .body("nodes", hasItem(address))
     }
 
     @Test
-    fun getClusterScheme() {
+    fun removeStorageNodeActuallyUpdatesScheme() {
+        val address = "localhost:8086"
+        val node = NodeRequest(address = address)
+        // Добавляем, чтобы гарантировать её наличие
+        RestAssured.given()
+            .contentType(ContentType.JSON)
+            .body(node)
+            .post("/scheme")
+            .then()
+            .statusCode(200)
+
+        // Удаляем
+        RestAssured.delete("/scheme/$address")
+            .then()
+            .statusCode(200)
+
+        // Проверяем, что её нет в схеме
+        RestAssured.get("/scheme")
+            .then()
+            .statusCode(200)
+            .body("nodes", not(hasItem(address)))
+    }
+
+    @Test
+    fun getClusterSchemeReturnsValidScheme() {
         RestAssured
             .get("/scheme")
             .then()
@@ -73,15 +85,29 @@ class MasterNodeApiTests {
     }
 
     @Test
-    fun updateShardCount() {
-        val req = ChangeShardRequest(shardCount = 4)
-        RestAssured
-            .given()
+    fun updateShardCountActuallyUpdatesScheme() {
+        // Получаем текущее число шардов
+        val oldVirtualNodes = RestAssured.get("/scheme")
+            .then()
+            .statusCode(200)
+            .extract()
+            .jsonPath()
+            .getInt("virtualNodes")
+
+        // Меняем число шардов (например, увеличиваем на 1)
+        val newShardCount = oldVirtualNodes + 1
+        val req = ChangeShardRequest(shardCount = newShardCount)
+        RestAssured.given()
             .contentType(ContentType.JSON)
             .body(req)
             .put("/shards")
             .then()
             .statusCode(200)
-            .body("message", containsString("done"))
+
+        // Проверяем, что число виртуальных нод обновилось
+        RestAssured.get("/scheme")
+            .then()
+            .statusCode(200)
+            .body("virtualNodes", equalTo(newShardCount))
     }
 }
